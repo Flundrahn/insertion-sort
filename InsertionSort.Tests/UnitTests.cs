e83133b31d43a8a852f7d0d5e03b9cc5a82b351a
Flundrahn <flundst@gmail.com>
2022-09-25 22:43:37 +0200

    integrate FredSort with counter of moves

--- InsertionSort.Tests/UnitTests.cs
+++ InsertionSort.Tests/UnitTests.cs
@@ -24,70 +24,125 @@ public class UnitTests
     }
 
     [Fact]
-    public void DoFredSort_should_sort_array_of_two()
+    public void insertionSort_should_sort_array_of_two()
     {
         // Arrange
         var testArray = new int[] { 2, 1 };
 
         // Act
-        var sorted = Result.DoFredSort(testArray);
+        var (_, sorted) = Result.insertionSort(testArray);
 
         // Assert
         sorted.Should().BeEquivalentTo(new int[] { 1, 2 }, options => options.WithStrictOrdering());
     }
 
     [Fact]
-    public void DoFredSort_should_sort_array_of_three()
+    public void insertionSort_should_sort_array_of_three()
     {
         // Arrange
         var testArray = new int[] { 3, 2, 1 };
 
         // Act
-        var sorted = Result.DoFredSort(testArray);
+        var (_, sorted) = Result.insertionSort(testArray);
 
         // Assert
         sorted.Should().BeEquivalentTo(new int[] { 1, 2, 3 }, options => options.WithStrictOrdering());
     }
 
     [Fact]
-    public void DoFredSort_should_sort_array_of_four()
+    public void insertionSort_should_sort_array_of_four()
     {
         // Arrange
         var testArray = new int[] { 4, 3, 2, 1 };
 
         // Act
-        var sorted = Result.DoFredSort(testArray);
+        var (_, sorted) = Result.insertionSort(testArray);
 
         // Assert
         sorted.Should().BeEquivalentTo(new int[] { 1, 2, 3, 4 }, options => options.WithStrictOrdering());
     }
 
     [Fact]
-    public void DoFredSort_should_handle_alrady_sorted()
+    public void insertionSort_should_handle_already_sorted()
     {
         // Arrange
         var testArray1 = new int[] { 1, 2, 3, 4 };
         var testArray2 = new int[] { 1, 2 };
+        var testArray3 = new int[] { 1, 1, 1, 2, 2};
 
         // Act
-        var sorted1 = Result.DoFredSort(testArray1);
-        var sorted2 = Result.DoFredSort(testArray2);
+        var (_, sorted1) = Result.insertionSort(testArray1);
+        var (_, sorted2) = Result.insertionSort(testArray2);
+        var (_, sorted3) = Result.insertionSort(testArray3);
 
         // Assert
-        sorted1.Should().BeEquivalentTo(new int[] { 1, 2, 3, 4 }, options => options.WithStrictOrdering());
-        sorted2.Should().BeEquivalentTo(new int[] { 1, 2 }, options => options.WithStrictOrdering());
+        sorted1.Should().BeEquivalentTo(testArray1, options => options.WithStrictOrdering());
+        sorted2.Should().BeEquivalentTo(testArray2, options => options.WithStrictOrdering());
+        sorted3.Should().BeEquivalentTo(testArray3, options => options.WithStrictOrdering());
     }
 
     [Fact]
-    public void DoFredSort_should_handle_multiple_same_values()
+    public void insertionSort_should_handle_multiple_same_values()
     {
         // Arrange
         var testArray1 = new int[] { 3, 2, 3, 4, 5 };
 
         // Act
-        var sorted1 = Result.DoFredSort(testArray1);
+        var (_, sorted) = Result.insertionSort(testArray1);
 
         // Assert
-        sorted1.Should().BeEquivalentTo(new int[] { 2, 3, 3, 4, 5 }, options => options.WithStrictOrdering());
+        sorted.Should().BeEquivalentTo(new int[] { 2, 3, 3, 4, 5 }, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void insertionSort_should_finish_faster_when_find_equal_value()
+    {
+        // Arrange
+        var testArray1 = new int[] { 1, 2, 3, 4, 5, 3 };
+
+        // Act
+        var (_, sorted) = Result.insertionSort(testArray1);
+
+        // Assert
+        sorted.Should().BeEquivalentTo(new int[] { 1, 2, 3, 3, 4, 5 }, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void insertionSort_should_handle_already_sorted_unique_numbers()
+    {
+        // Arrange
+        var testArray1 = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        var (numberOfMoves, _) = Result.insertionSort(testArray1);
+
+        // Assert
+        numberOfMoves.Should().Be(0);
+    }
+
+    [Fact]
+    public void insertionSort_should_handle_ordered_multiples_of_same()
+    {
+        // Arrange
+        var testArray1 = new int[] { 1, 1, 1, 2, 2 };
+
+        // Act
+        var (numberOfMoves, _) = Result.insertionSort(testArray1);
+
+        // Assert
+        numberOfMoves.Should().Be(0);
+    }
+
+    [Fact]
+    public void insertionSort_should_handle_unordered_multiples()
+    {
+        // Arrange
+        var testArray2 = new int[] { 2, 1, 3, 1, 2 };
+
+        // Act
+        var (numberOfMoves, _) = Result.insertionSort(testArray2);
+
+        // Assert
+        numberOfMoves.Should().Be(4);
     }
 }
--- InsertionSort/Result.cs
+++ InsertionSort/Result.cs
@@ -1,23 +1,82 @@
 public static class Result
 {
-    // NOTE Returns number of moves
-    public static int insertionSort(int[] arr)
+    public static (int, int[]) insertionSort(int[] arr)
     {
         int numberOfMoves = 0;
+        IEnumerable<int> sorted = arr.Take(1);
+
         for (var i = 1; i < arr.Length; i++)
         {
-            var currentInteger = arr[i];
-            var j = i - 1;
-            while (j >= 0 && arr[j] > currentInteger)
+            var current = arr[i];
+            // NOTE This may be unlikely enough that it does not pay off to add one extra if here, one extra if for each number
+            if (current >= sorted.ElementAt(i - 1))
             {
-                arr[j + 1] = arr[j];
-                j--;
+                sorted = sorted.Concat(new int[] { current });
+                continue;
             }
-            arr[j + 1] = currentInteger;
-            numberOfMoves += i - (j + 1);
+
+            var upperIndex = i;
+            var middleIndex = i / 2;
+            var lowerIndex = 0;
+
+            while (upperIndex - lowerIndex > 0)
+            {
+                if (current < sorted.ElementAt(middleIndex)) // NOTE Consider using List<int> instead
+                {
+                    upperIndex = middleIndex;
+                }
+                else if (current > sorted.ElementAt(middleIndex))
+                {
+                    lowerIndex = middleIndex + 1;
+                }
+                else
+                {
+                    goto insert;
+                }
+
+                middleIndex = (upperIndex + lowerIndex) / 2;
+            }
+
+        insert:
+            // NOTE Previously also had this && (middleIndex + 1) != i which should never happen since everything is sorted by size, and in beginning of algorithm the leftmost element is checked. Keep out unless remove first check.
+            while (current == sorted.ElementAt(middleIndex))
+            {
+                middleIndex += 1;
+            }
+
+            sorted = sorted.Take(middleIndex)
+                .Concat(new int[] { current })
+                .Concat(sorted
+                    .Skip(middleIndex)
+                    .Take(i - middleIndex));
+
+            numberOfMoves += i - middleIndex;
         }
-        return numberOfMoves;
+        return (numberOfMoves, sorted.ToArray());
     }
+
+    // 2 1 3 1 2
+    // 1 2 3 1 2 --> (1 - 0) = 1 move
+    // 1 2 3 1 2 --> (2 - 2) = 0 move
+    // 1 1 2 3 2 --> (3 - 1) = 2 move
+
+    // public static int insertionSort(int[] arr)
+    // {
+    //     int numberOfMoves = 0;
+    //     for (var i = 1; i < arr.Length; i++)
+    //     {
+    //         var currentInteger = arr[i];
+    //         var j = i - 1;
+    //         while (j >= 0 && arr[j] > currentInteger)
+    //         {
+    //             arr[j + 1] = arr[j];
+    //             j--;
+    //         }
+    //         arr[j + 1] = currentInteger;
+    //         numberOfMoves += i - (j + 1);
+    //     }
+    //     return numberOfMoves;
+    // }
 
     public static int[] DoInsertionSort(int[] arr)
     {
@@ -77,49 +136,6 @@ public static class Result
         }
         return sorted!.ToArray();
     }
-
-    public static int GetMiddleIndex(int upperIndex, int lowerIndex) => (upperIndex + lowerIndex) / 2;
-
-    // while (middleIndex > lowerIndex)
-    //         {
-    //             middleIndex = (upperIndex + lowerIndex) / 2;
-
-    //             if (arr[middleIndex] > current)
-    //             {
-    //                 upperIndex = middleIndex;
-    //             }
-    //             else if (arr[middleIndex] < current)
-    //             {
-    //                 lowerIndex = middleIndex;
-    //             }
-    //             else if (arr[middleIndex] == current)
-    //             {
-    //                 // found exactly equal, place there
-    //             }
-    //         }
-
-    //         if (middleIndex == 0)
-    //         {
-    //             if (arr[middleIndex] > current)
-    //             {
-    //                 newLeftArray = new int[] {current}
-    //                     .Concat(newLeftArray[middleIndex..upperIndex])
-    //                     .ToArray();
-    //             }
-    //             else
-    //             {
-    //                 newLeftArray = newLeftArray[0..middleIndex]
-    //                     .Concat(new int[] {current})
-    //                     .Concat(newLeftArray[middleIndex..])
-    //                     .ToArray();
-    //             }
-    //         }
-    //         // 
-    //         newLeftArray = arr[0..middleIndex]
-    //             .Concat(new int[] { current })
-    //             .Concat(arr[(middleIndex + 1)..])
-    //             .ToArray();
-    //     }
 
     public static int insertionSortOld(int[] arr)
     {
